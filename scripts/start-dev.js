99a992b7b612d50cc1215744d9aa743efc8ad178
vivekkumar15 <kmr.vivek15@gmail.com>
2025-04-23 12:57:07 +0530

    detach mode dev server

---
+++ scripts/start-dev.js
@@ -0,0 +1,11 @@
+const { spawn } = require('child_process')
+
+// spawn in a new process group, inherit stdout/stderr but ignore stdin
+const child = spawn('yarn', ['run','dev'], {
+  detached: true,
+  stdio: ['ignore', 'inherit', 'inherit']
+})
+
+child.unref()
+console.log('🛫 Dev server launched in background')
+
