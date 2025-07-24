2322ca1a387a43b30a05df8503a76c6d61c218d2
Vivek <42060544+vivekkumar15@users.noreply.github.com>
2025-07-24 12:22:14 +0530

    icp recovery (#21)

--- src/recoveryTransaction/near.ts
+++ src/recoveryTransaction/near.ts
@@ -1,49 +1,10 @@
-import { connect, keyStores, transactions, utils } from 'near-api-js';
+import { connect, keyStores, transactions } from 'near-api-js';
 import { PublicKey } from 'near-api-js/lib/utils';
 import { KeyType } from 'near-api-js/lib/utils/key_pair';
-import { randomBytes } from 'crypto';
 import { sha256 } from 'js-sha256';
 import bs58 from 'bs58';
-import {
-  bytesToNumberLE,
-  concatBytes,
-  numberToBytesLE,
-} from '@noble/curves/abstract/utils';
+import { eddsaSign } from './signature';
 
-const sha512 = async (...messages: Uint8Array[]) => {
-  const buffer = concatBytes(...messages);
-  const digest = await crypto.subtle.digest('SHA-512', buffer);
-  return new Uint8Array(digest);
-};
-
-export async function eddsaSign(
-  privateKey: bigint,
-  message: Uint8Array | string,
-  hasher: (...msgs: Uint8Array[]) => Promise<Uint8Array> = sha512
-): Promise<Uint8Array> {
-  const { ExtendedPoint, CURVE: edCURVE, etc } = await import('@noble/ed25519');
-
-  const messagesBytes =
-    typeof message === 'string' ? Buffer.from(message, 'hex') : message;
-  const messageBytes = concatBytes(messagesBytes);
-  const seed = randomBytes(32);
-  const privateKeyBytes = numberToBytesLE(privateKey, 32);
-
-  const nonceDigest = await hasher(seed, privateKeyBytes, messageBytes);
-  const nonce = etc.mod(bytesToNumberLE(nonceDigest), edCURVE.n);
-
-  const PK = ExtendedPoint.BASE.mul(privateKey);
-  const serializedPK = PK.toRawBytes();
-
-  const R = ExtendedPoint.BASE.mul(nonce);
-  const serializedR = R.toRawBytes();
-
-  const hramDigest = await hasher(serializedR, serializedPK, messageBytes);
-  const hram = etc.mod(bytesToNumberLE(hramDigest), edCURVE.n);
-
-  const s = etc.mod(hram * privateKey + nonce, edCURVE.n);
-  return concatBytes(serializedR, numberToBytesLE(s, 32));
-}
 
 /**
  * Gets the latest nonce for a given public key by directly querying the RPC endpoint
